6dd47c2e16440db745309e70689c543aceb2a2d6
iCAMPAGNE <development@icampagne.nl>
2022-09-05 20:35:29 +0200

    Migration to Angular 11, First version works on tablet.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,6 +7,7 @@ interface Card {
     type:string;
     blackType:boolean;
     pileNr:number;
+    searching:boolean;
     turning:boolean;
     turned:boolean;
 }
@@ -58,7 +59,7 @@ export class HomeComponent implements OnInit {
               imageUrl += '.png';
 
               const id = this.nr++;
-              this.cards.push({id: id, value: value, imageUrl: imageUrl, type: type, blackType: blackType, pileNr: 5, turning: false, turned: false});
+              this.cards.push({id: id, value: value, imageUrl: imageUrl, type: type, blackType: blackType, pileNr: 5, searching: false, turning: false, turned: false});
               this.spread.push(this.cards.length - 1);
           }
       });
@@ -86,47 +87,86 @@ export class HomeComponent implements OnInit {
   }
 
   draaiom(event: Event, card:Card) {
+      if (this.piles.some(pile => pile.cards.some(card => card.searching))) {
+          console.log('draaiom occupied');
+          return;
+      }
+      card.searching = true;
       card.turning = true;
       event.stopPropagation(); // Prevent exectution empty stock-pile click
       const flipCardElement = this.elementRef.nativeElement.querySelector('#card-' + card.id);
-      flipCardElement.animate([{ transform: 'rotateY(180deg) '}, { transform: 'translateX(-150px)' } ], { duration: 500 });
+      flipCardElement.animate([{ transform: 'rotateY(180deg) '}, { transform: 'translateX(-10vw)' } ], { duration: 200 });
       setTimeout(() => {
           card.turned = true;
           this.piles[5].cards.pop();
           card.pileNr = 4;
           this.piles[4].cards.push(card);
           card.turning = false;
-      }, 490);
+          card.searching = false;
+      }, 190);
   }
 
-  rechargePile(pileNr:number) {
-      if (this.piles[pileNr].cards.find(c => c.turning)) return;
+    turnAround() {
+        const card:Card = this.piles[5].cards[this.piles[5].cards.length - 1];
+        if (this.piles.some(pile => pile.cards.some(card => card.searching))) {
+            console.log('turnAround occupied');
+            return;
+        }
+        card.searching = true;
+        card.turning = true;
+        this.piles[5].cards.pop();
+        card.pileNr = 4;
+        card.turned = true;
+        this.piles[4].cards.push(card);
+        card.turning = false;
+        card.searching = false;
+        this.checkGameStatus();
+    }
+
+  rechargePile() {
+      if (this.piles.some(pile => pile.cards.some(card => card.searching))) return;
+      if (this.piles[5].cards.find(c => c.turning)) {
+          console.log('rechargePile done, going to checkGameStatus');
+          this.checkGameStatus();
+          return;
+      }
       if (this.piles[4].cards.length > 0) {
           const card = this.piles[4].cards[this.piles[4].cards.length - 1];
           if (card) {
               card.turned = false;
               card.turning = true;
               const flipCardElement = this.elementRef.nativeElement.querySelector('#card-' + card.id);
-              flipCardElement.animate([ { transform: 'rotateY(-180deg)' }, { transform: 'translateX(150px)' } ], { duration: 50 });
+              flipCardElement.animate([ { transform: 'rotateY(-180deg)' }, { transform: 'translateX(10vw)' } ], { duration: 50 });
               setTimeout(() => {
                   card.turning = false;
                   this.piles[4].cards.pop();
                   card.pileNr = 5;
                   this.piles[5].cards.push(card);
-                  this.rechargePile(pileNr);
+                  this.rechargePile();
               }, 49);
+          } else {
+              this.checkGameStatus();
           }
+      } else {
+          this.checkGameStatus();
       }
   }
 
 
-  cardClick(pileNr:number, card:Card): boolean {
+  cardClick(pileNr:number, card:Card, first4PilesOnly:boolean): boolean {
+      if (this.piles.some(pile => pile.cards.some(card => card.searching))) {
+          console.log('cardClick occupied');
+          return false;
+      }
     if (!card || !card.turned) {
       return false;
     }
+    card.searching = true;
 
     for (let tryPileNr of [0,1,2,3,6,7,8,9,10,11,12]) {
+        console.log(pileNr + ': tryPileNr = ' + tryPileNr);
         if (tryPileNr === pileNr) continue;
+        if (first4PilesOnly && tryPileNr > 3) continue;
         const bottommostCard:Card = this.piles[tryPileNr].cards[this.piles[tryPileNr].cards.length - 1];
         console.log('candidate');
         if (!((tryPileNr < 4 && ((card.value === 1 && !bottommostCard) ||
@@ -134,7 +174,7 @@ export class HomeComponent implements OnInit {
               || (tryPileNr >= 6 && !bottommostCard && card.value === 13)
               || (tryPileNr >= 6 && (bottommostCard && (card.value === bottommostCard.value - 1) && card.blackType != bottommostCard.blackType)))) continue;
 
-        const pileElement = this.elementRef.nativeElement.querySelector('#pile-' + tryPileNr);
+        console.log('match');
 
         const numberInPile:number = this.piles[pileNr].cards.indexOf(card);
         const cards:Card[] = [];
@@ -142,12 +182,14 @@ export class HomeComponent implements OnInit {
         for (let movingCardId = numberInPile; movingCardId < this.piles[pileNr].cards.length; movingCardId++) {
             const c:Card = this.piles[pileNr].cards[movingCardId];
             cards.push(c);
+            const pileElement = this.elementRef.nativeElement.querySelector('#pile-' + tryPileNr);
             const cardElement = this.elementRef.nativeElement.querySelector('#card-' + c.id);
             const moveHorizontal:number = pileElement.offsetLeft - cardElement.getBoundingClientRect().left;
-            const moveVertical:number = pileElement.offsetTop + (tryPileNr > 4 ? this.piles[tryPileNr].cards.length * 40 + 40*offset++: 0) - cardElement.getBoundingClientRect().top;
-            cardElement.animate([{ transform: 'translate(' + moveHorizontal + 'px,' + moveVertical + 'px)'}], { duration: 200 });
+            const moveVertical:number = pileElement.offsetTop + (tryPileNr > 4 ? this.piles[tryPileNr].cards.length * 50 + 50*offset++: 0) - cardElement.getBoundingClientRect().top + 5;
+            cardElement.animate([{ transform: 'translate(' + moveHorizontal/12 + 'vw,' + moveVertical/12 + 'vh)'}], { duration: 200 });
         }
         setTimeout(() => {
+            console.log('moving card(s)');
             for (let c of cards) {
                 this.piles[pileNr].cards.pop();
                 c.pileNr = tryPileNr;
@@ -163,16 +205,59 @@ export class HomeComponent implements OnInit {
                 setTimeout(() => {
                     nextCard.turned = true;
                     nextCard.turning = false;
-
-                    // Has game ended?
-                    this.gameEnded = !this.piles.some((pile, index) => index >= 6 && pile.cards.some(card => !card.turned));
-                    console.log('gameEnded ' + this.gameEnded);
-
+                    card.searching = false;
+                    this.checkGameStatus();
                 }, 190);
+            } else {
+                card.searching = false;
+                this.checkGameStatus();
             }
         }, 190);
         return true;
     }
+    card.searching = false;
     return false;
+  }
+
+  checkGameStatus() {
+
+      // Has game ended?
+      setTimeout(() => {
+          console.log('Has game ended?');
+          if (!this.piles.some((pile, index) => index >= 6 && pile.cards.some(card => !card.turned))) {
+              console.log('Game about to end');
+              if (this.piles.some((pile, index) => index >= 4 && pile.cards.length > 0)) {
+                  console.log('ending game');
+                  let nextPileNr = 6;
+                  while (this.piles[nextPileNr].cards.length === 0 || !this.cardClick(nextPileNr, this.piles[nextPileNr].cards[this.piles[nextPileNr].cards.length - 1], true)) {
+                      nextPileNr = nextPileNr === 12 ? 4 : nextPileNr + 1;
+                      console.log('Attempt failed, next pile is ' + nextPileNr + '  ' + (this.piles[5].cards.length === 0));
+                      if (nextPileNr === 5) {
+                          if (this.piles[5].cards.length === 0) {
+                              this.rechargePile();
+                          } else {
+                              this.turnAround();
+                          }
+                          return;
+                      }
+                  }
+              } else {
+                  this.gameEnded = true;
+                  console.log('gameEnded ' + this.gameEnded);
+              }
+          }
+      }, 10);
+
+  }
+
+  startGame() {
+      this.cards = [];
+      this.spread = [];
+      this.nr = 0;
+      this.gameEnded = false;
+
+      this.piles = [];// new Array(13).fill({cards: []});
+
+      this.ngOnInit();
   }
 }
