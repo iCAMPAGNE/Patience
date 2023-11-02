445f0c42c9c09ba1e43a7883d58f638977252eda
iCAMPAGNE <development@icampagne.nl>
2023-11-02 18:22:38 +0100

    Replaced timers by Promises on animation.finished

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -100,7 +100,6 @@ export class HomeComponent implements OnInit {
         }
 
         // Read and increase number of played games using cookies.
-
         if (typeof(Storage) !== "undefined") { // Check browser support
             const gamesPlayedCookie = localStorage.getItem(GamesPlayedCookie);
             if (gamesPlayedCookie) {
@@ -121,15 +120,17 @@ export class HomeComponent implements OnInit {
         const pile4Element = this.elementRef.nativeElement.querySelector('#pile-4');
         const pile5Element = this.elementRef.nativeElement.querySelector('#pile-5');
         const moveHorizontal: number = pile4Element.getBoundingClientRect().left - pile5Element.getBoundingClientRect().left;
+
         flipCardElement.animate([{transform: 'rotateY(180deg)'}, {transform: 'translateX(' + moveHorizontal / (this.screenWidth / 100) + 'vw)'}], {duration: 300});
-        setTimeout(() => {
-            card.turned = true;
-            this.piles[5].cards.pop();
-            this.piles[4].cards.push(card);
-            card.pileNr = 4;
-            card.turning = false;
-            card.searching = false;
-        }, 280);
+        Promise.all(flipCardElement.getAnimations().map((animation:any) => animation.finished)).then(() => {
+              card.turned = true;
+              this.piles[5].cards.pop();
+              this.piles[4].cards.push(card);
+              card.pileNr = 4;
+              card.turning = false;
+              card.searching = false;
+          },
+        );
     }
 
     private _turnAround() {
@@ -164,13 +165,13 @@ export class HomeComponent implements OnInit {
             card.turning = true;
             const flipCardElement = this.elementRef.nativeElement.querySelector('#card-' + card.id);
             flipCardElement.animate([{transform: 'rotateY(-180deg)'}, {transform: 'translateX(10vw)'}], {duration: 100});
-            setTimeout(() => {
+            Promise.all(flipCardElement.getAnimations().map((animation:any) => animation.finished)).then(() => {
                 card.turning = false;
                 this.piles[4].cards.pop();
                 card.pileNr = 5;
                 this.piles[5].cards.push(card);
                 this._rechargePile();
-            }, 95);
+            });
         } else {
             this._checkGameStatus();
             this.rechargingPile = false;
@@ -200,7 +201,6 @@ export class HomeComponent implements OnInit {
             const numberInPile: number = this.piles[pileNr].cards.indexOf(card);
             const cards: Card[] = [];
             let numberOfCardsToBeMoved = 0;
-            let duration = 0;
             for (let movingCardId = numberInPile; movingCardId < this.piles[pileNr].cards.length; movingCardId++) {
                 const c: Card = this.piles[pileNr].cards[movingCardId];
                 c.moving = true;
@@ -222,10 +222,10 @@ export class HomeComponent implements OnInit {
                         moveVertical = bottomCardOfTryPileElement.getBoundingClientRect().top + offsetPerCard + offsetPerCard * numberOfCardsToBeMoved++ - cardElement.getBoundingClientRect().top;
                     }
                 }
-                duration = Math.sqrt(Math.pow(moveHorizontal,2) + Math.pow(moveVertical, 2)) / 2;
+                const duration = Math.sqrt(Math.pow(moveHorizontal,2) + Math.pow(moveVertical, 2)) / 2;
                 cardElement.animate([{transform: 'translate(' + moveHorizontal / (this.screenWidth / 100) + 'vw,' + moveVertical / (this.screenHeight / 100) + 'vh)'}], {duration: duration});
             }
-            setTimeout(() => {
+            Promise.all(document.getAnimations().map((animation:any) => animation.finished)).then(() => {
                 // make the movement of the card(s) final.
                 for (let c of cards) {
                     this.piles[pileNr].cards.pop();
@@ -240,17 +240,17 @@ export class HomeComponent implements OnInit {
                     nextCard.turning = true;
                     const flipCardElement = this.elementRef.nativeElement.querySelector('#card-' + nextCard.id + '-turning');
                     flipCardElement.animate([{transform: 'rotateY(180deg) '}], {duration: 300});
-                    setTimeout(() => {
+                    Promise.all(flipCardElement.getAnimations().map((animation:any) => animation.finished)).then(() => {
                         nextCard.turned = true;
                         nextCard.turning = false;
                         card.searching = false;
                         this._checkGameStatus();
-                    }, 295);
+                    });
                 } else {
                     card.searching = false;
                     this._checkGameStatus();
                 }
-            }, duration / 1.1);
+            });
             return true;
         }
         card.searching = false;
@@ -258,40 +258,35 @@ export class HomeComponent implements OnInit {
     }
 
     private _checkGameStatus() {
-
         // Has game ended?
-        setTimeout(() => {
-            if (!this.piles.some((pile, index) => index >= 6 && pile.cards.some(card => !card.turned))) {
-                if (this.piles.some((pile, index) => index >= 4 && pile.cards.length > 0)) {
-                    let nextPileNr = 6;
-                    while (this.piles[nextPileNr].cards.length === 0 || !this.cardClick(nextPileNr, this.piles[nextPileNr].cards[this.piles[nextPileNr].cards.length - 1], true)) {
-                        nextPileNr = nextPileNr === 12 ? 4 : nextPileNr + 1;
-                        if (nextPileNr === 5) {
-                            if (this.piles[5].cards.length === 0) {
-                                this._rechargePile();
-                            } else {
-                                this._turnAround();
-                            }
-                            return;
+        if (!this.piles.some((pile, index) => index >= 6 && pile.cards.some(card => !card.turned))) {
+            if (this.piles.some((pile, index) => index >= 4 && pile.cards.length > 0)) {
+                let nextPileNr = 6;
+                while (this.piles[nextPileNr].cards.length === 0 || !this.cardClick(nextPileNr, this.piles[nextPileNr].cards[this.piles[nextPileNr].cards.length - 1], true)) {
+                    nextPileNr = nextPileNr === 12 ? 4 : nextPileNr + 1;
+                    if (nextPileNr === 5) {
+                        if (this.piles[5].cards.length === 0) {
+                            this._rechargePile();
+                        } else {
+                            this._turnAround();
                         }
+                        return;
                     }
-                } else {
-                    // Read and increase number of games won using cookies.
-
-                    if (typeof(Storage) !== "undefined") { // Check browser support
-                        const gamesWonCookie = localStorage.getItem(GamesWonCookie);
-                        if (gamesWonCookie) {
-                            this.gamesWon = +gamesWonCookie;
-                        }
-                        localStorage.setItem(GamesWonCookie, '' + (++this.gamesWon).toString());
-                    } else {
-                        console.log("Sorry, your browser does not support Web Storage...");
-                    }
-                    this.gameEnded = true;
                 }
+            } else {
+                // Read and increase number of games won using cookies.
+                if (typeof(Storage) !== "undefined") { // Check browser support
+                    const gamesWonCookie = localStorage.getItem(GamesWonCookie);
+                    if (gamesWonCookie) {
+                        this.gamesWon = +gamesWonCookie;
+                    }
+                    localStorage.setItem(GamesWonCookie, '' + (++this.gamesWon).toString());
+                } else {
+                    console.log("Sorry, your browser does not support Web Storage...");
+                }
+                this.gameEnded = true;
             }
-        }, 10);
-
+        }
     }
 
     startGame() {
@@ -299,9 +294,7 @@ export class HomeComponent implements OnInit {
         this.spread = [];
         this.nr = 0;
         this.gameEnded = false;
-
         this.piles = [];
-
         this.ngOnInit();
     }
 }
